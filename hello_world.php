54bbaa6bd2668f68a34261dd594e95267aeefa3d
shuman7 <31026019+shuman7@users.noreply.github.com>
2017-10-25 01:51:21 +0900

    きほんのきほんChapter5

--- hello_world.php
+++ hello_world.php
@@ -1,3 +1,16 @@
 <?php
     echo"Hello World!";
 ?>
+<html>
+<body>
+<form method="GET" action="example5-1-1.php">
+お名前:<input type="text" name="username">
+//プログラムから「username」とい名称で、ここに入力された値を取得できる
+<input type="submit" value="送信">
+//入力フォームの内容をサーバに送信するにはsubmitボタンが必要
+//type属性にsubmitという値を指定したものがsubmitボタン
+
+
+</form>
+</body>
+</html>
